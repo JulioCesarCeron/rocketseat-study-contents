a7ea7c42f3c2dc2be54c3567283e09c023bc0193
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-06-15 15:05:37 -0300

    react-native structure

---
+++ instagram-clone/instarocket/src/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Routes from './routes';
+
+
+export default function App() {
+  return <Routes />
+}  
