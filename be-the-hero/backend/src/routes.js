3ec9e91257ffc9d8eb66bbc23ac40ab578043470
julio.ceron <julio.ceorn@gmail.com>
2020-03-25 22:00:23 -0300

    backend application

--- be-the-hero/backend/src/routes.js
+++ be-the-hero/backend/src/routes.js
@@ -1,12 +1,23 @@
 const express = require('express');
 
+const OngCrontroller = require('./controllers/OngCrontroller');
+const IncidentCrontroller = require('./controllers/IncidentCrontroller');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
 const routes = express.Router();
 
-routes.post('/users', (req, res) => {
-	return res.json({
-		evento: 'Semana OmniStack',
-		aluno: "Julio"
-	})
-});
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngCrontroller.index);
+routes.post('/ongs', OngCrontroller.create);
+
+routes.get('/profile/', ProfileController.index);
+
+routes.get('/incidents', IncidentCrontroller.index);
+routes.post('/incidents', IncidentCrontroller.create);
+routes.delete('/incidents/:id', IncidentCrontroller.delete);
+
 
 module.exports = routes;
