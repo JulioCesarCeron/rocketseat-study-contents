9d27a06d39dd5d50cba28d30080693329a46fa46
julio.ceron <julio.ceorn@gmail.com>
2020-06-06 18:30:08 -0300

    adds HTML of first page

--- nlw/web/src/App.tsx
+++ nlw/web/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
   return (
 		<div>
-			<Header title="Hello world" />
-
-			<h1>Conteúdo da Aplicação</h1>
+			<Home />
 		</div>
   );
 }
