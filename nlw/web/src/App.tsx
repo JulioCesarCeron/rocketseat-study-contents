c114a9bfa46224be63736555a3a9763480b764a4
julio.ceron <julio.ceorn@gmail.com>
2020-06-06 17:04:10 -0300

    add Header component

--- nlw/web/src/App.tsx
+++ nlw/web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-		<h1>Hello World!!</h1>
+		<div>
+			<Header title="Hello world" />
+
+			<h1>Conteúdo da Aplicação</h1>
+		</div>
   );
 }
 
