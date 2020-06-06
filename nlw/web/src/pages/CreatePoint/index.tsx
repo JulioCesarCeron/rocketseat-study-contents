70d57d14645687d82692b11df0c4f5bcaa282875
julio.ceron <julio.ceorn@gmail.com>
2020-06-06 19:45:33 -0300

    adds endpoint to get all collect items

--- nlw/web/src/pages/CreatePoint/index.tsx
+++ nlw/web/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import './styles.css';
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+
+
+interface Item {
+	id: number;
+	title: string;
+	image_url: string;
+}
 
 const CreatePoint = () => {
+	const [items, setitems] = useState<Item[]>([]);
+
+	useEffect(() => {
+		api.get('/items').then((response) => {
+			setitems(response.data);
+		});
+	}, []);
+
 	return (
 		<div id="page-create-point">
 			<header>
@@ -83,38 +99,15 @@ const CreatePoint = () => {
 						<h2>Ítens de Coleta</h2>
 						<span>Selecione um ou mais ítens abaixo</span>
 					</legend>
-
+				
 					<ul className="items-grid">
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-							<span>Óleo de cozinha</span>
-						</li>
-						
-						<li className="selected">
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-							<span>Óleo de cozinha</span>
-						</li>
-						
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-							<span>Óleo de cozinha</span>
-						</li>
-						
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-							<span>Óleo de cozinha</span>
-						</li>
-						
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-							<span>Óleo de cozinha</span>
-						</li>
-						
-						<li>
-							<img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-							<span>Óleo de cozinha</span>
-						</li>
-						
+
+						{items.map(item => (
+							<li key={item.id}>
+								<img src={item.image_url} alt={item.title} />
+								<span>{item.title}</span>
+							</li>
+						))}			
 					</ul>
 				</fieldset>
 
