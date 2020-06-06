57d7d7f030f230cf463564fccae60aadab553d5c
julio.ceron <julio.ceorn@gmail.com>
2020-06-06 16:47:01 -0300

    clean structure

--- nlw/web/src/App.tsx
+++ nlw/web/src/App.tsx
@@ -3,11 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!!</h1>
-      </header>
-    </div>
+		<h1>Hello World!!</h1>
   );
 }
 
