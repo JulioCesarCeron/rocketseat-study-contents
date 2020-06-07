2eda4bc0a71061ad98d69f164ba666a1790746da
julio.ceron <julio.ceorn@gmail.com>
2020-06-07 12:55:56 -0300

    add Detail page

--- nlw/mobile/src/pages/Detail/index.tsx
+++ nlw/mobile/src/pages/Detail/index.tsx
@@ -1,11 +1,55 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Detail = () => {
+	const navigation = useNavigation();
+
+	function handleNavigateBack() {
+		navigation.goBack();
+	}
+
 	return(
-		<View>
-			<Text>Detail</Text>
-		</View>
+		<SafeAreaView style={{ flex: 1 }}>
+			<View style={styles.container}>
+				<TouchableOpacity onPress={handleNavigateBack} >
+					<Icon name="arrow-left" size={20} color="#34cb79" />
+				</TouchableOpacity>
+
+				<Image
+					style={styles.pointImage}
+					source={{
+						uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+					}}
+				/>
+				<Text style={styles.pointName}>
+					Mercado teste
+				</Text>
+				<Text style={styles.pointItems}>Lâmpadas, Óleo de cozinha</Text>
+
+				<View style={styles.address}>
+					<Text style={styles.addressTitle}>Endereço</Text>
+					<Text style={styles.addressContent}>Florianópolis, SC</Text>
+				</View>
+			</View>
+
+			<View style={styles.footer}>
+				<RectButton style={styles.button} onPress={() => {}}>
+					<FontAwesome name="whatsapp" size={20} color="#FFF">
+						<Text style={styles.buttonText}>Whatsapp</Text>
+					</FontAwesome>
+				</RectButton>
+
+				<RectButton style={styles.button} onPress={() => {}}>
+					<Icon name="mail" size={20} color="#FFF">
+						<Text style={styles.buttonText}>E-mail</Text>
+					</Icon>
+				</RectButton>
+			</View>
+		</SafeAreaView>
 	);
 };
 
@@ -13,7 +57,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20,
+    paddingTop: 20 + Constants.statusBarHeight,
   },
 
   pointImage: {
@@ -59,7 +103,7 @@ const styles = StyleSheet.create({
   footer: {
     borderTopWidth: StyleSheet.hairlineWidth,
     borderColor: '#999',
-    paddingVertical: 20,
+		paddingVertical: 20,
     paddingHorizontal: 32,
     flexDirection: 'row',
     justifyContent: 'space-between'
